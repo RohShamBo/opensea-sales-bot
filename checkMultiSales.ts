9b8a5f979b1d1e0ad3ed4079d91d7ed17837fa7a
RohShamBo <80442111+RohShamBo@users.noreply.github.com>
2021-08-10 15:44:44 -0400

    Update checkMultiSales.ts

--- checkMultiSales.ts
+++ checkMultiSales.ts
@@ -51,7 +51,7 @@ async function main() {
   { 
     console.log(collection_slugs[i]); 
     console.log(collection_adds[i]); 
-                          
+    if(collection_slugs[i] != '') {                    
   	const openSeaResponse = await fetch(
     		"https://api.opensea.io/api/v1/events?" + new URLSearchParams({
      		 offset: '0',
@@ -71,6 +71,7 @@ async function main() {
       			return channel.send(message)
     		})
   	); 
+    }
   }
 }
 
