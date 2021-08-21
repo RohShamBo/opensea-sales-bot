22cc5fe6fa726ec0d7f9d1e9804d3c18e2868ee1
RohShamBo <80442111+RohShamBo@users.noreply.github.com>
2021-08-21 14:59:40 -0400

    Update restartBot.ts

--- restartBot.ts
+++ restartBot.ts
@@ -4,6 +4,11 @@ import Discord, { TextChannel } from 'discord.js';
 const discordBot = new Discord.Client();
 console.log("Enable restart")
 
+discordBot.on('ready', async () => {
+      const channel = await discordBot.channels.fetch(process.env.DISCORD_CHANNEL_ID!);
+      resolve(channel as TextChannel);
+    });
+
 discordBot.send('Resetting...')
     .then(msg => discordBot.destroy())
     .then(() => discordBot.login(process.env.DISCORD_BOT_TOKEN));
