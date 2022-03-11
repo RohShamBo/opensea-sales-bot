a7736c3392869bbd15666fb6079c459c71d71805
RohShamBo <80442111+RohShamBo@users.noreply.github.com>
2022-03-10 21:53:57 -0500

    Update checkSales.ts

--- checkSales.ts
+++ checkSales.ts
@@ -67,7 +67,7 @@ async function main() {
 		
   await Promise.all(
     openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
-      var timestamp = Math.round(new Date(sale.transaction.timestamp) / 1000)
+      var timestamp = Math.round(new Date(sale?.transaction?.timestamp) / 1000)
 	  if (timestamp > hoursAgo) {
 	      var buyer_name;
 	      var seller_name;
